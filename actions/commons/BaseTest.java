5f9b0febe179e6fe61f3fcd6258231e65e013fd6
lvcu <levancu190296@gmail.com>
2022-07-25 01:23:23 +0700

    apply headless browser, WebDriverMnagager:auto update driver version

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,9 +1,15 @@
 package commons;
 
+import java.sql.DriverManager;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 	WebDriver driver;
@@ -12,23 +18,41 @@ public class BaseTest {
 	protected WebDriver getBrowserDriver(String browserName) {
 		if(browserName.equals("firefox")) {
 			System.out.println("run on " + browserName);
-			System.setProperty("webdriver.gecko.driver",  projectPath +"\\browserDrivers\\geckodriver.exe");
+			//System.setProperty("webdriver.gecko.driver",  projectPath +"\\browserDrivers\\geckodriver.exe");
+			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
-			
+		}else if(browserName.equals("h_firefox")) {
+			System.out.println("run on " + browserName);
+			WebDriverManager.firefoxdriver().setup();
+			FirefoxOptions options = new FirefoxOptions();
+			options.addArguments("--headless"); 
+			options.addArguments("window-size=1920x1080");
+			driver = new FirefoxDriver(options);
 		}else if(browserName.equals("chrome")) {
 			System.out.println("run on " + browserName);
-			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			//System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
-			
+		}else if(browserName.equals("h_chrome")) {
+			System.out.println("run on " + browserName);
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("headless");
+			options.addArguments("window-size=1920x1080");
+			driver = new ChromeDriver(options);
 		}else if(browserName.equals("edge")) {
 			System.out.println("run on " + browserName);
-			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
-			driver = new EdgeDriver();
+			WebDriverManager.edgedriver().setup();
 			
+			//System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+			driver = new EdgeDriver();
 		}else {
 			throw new RuntimeException("Browser name invalid");
 		}
 		return driver;
+		// de truyen 1 version minh mon muon
+		//WebDriverManager.chromedriver().browserVersion("94.5.7").setup();
+		
 	}
 	
 }
