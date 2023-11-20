57a6c0daae5f32a0ace3f19627f49858af191b9b
lvcu <levancu190296@gmail.com>
2023-11-21 00:29:02 +0700

    lvcu 21-11

--- javaTester/javaBasic/Topic_17_Assert.java
+++ javaTester/javaBasic/Topic_17_Assert.java
@@ -1,4 +1,8 @@
+
 package javaBasic;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,18 +16,21 @@ import org.testng.annotations.Test;
 
 import commons.BaseTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
-
+ 
 @Listeners(commons.MethodListener.class)
-public class Topic_17_Check_Environment extends BaseTest {
+public class Topic_17_Assert extends BaseTest {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 
 	
 	@BeforeClass
 	public void beforeClass() {
+		//WebDriverManager.chromedriver().setup();
 		WebDriverManager.firefoxdriver().setup();
 		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		//System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
 		driver = new FirefoxDriver();
+		//driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/");
@@ -31,19 +38,27 @@ public class Topic_17_Check_Environment extends BaseTest {
 
 	@Test
 	public void TC_01_ValidateCurrentUrl() {
+		System.out.println("Assert 01");
 		log.info("verify equal 01");
 		String loginPageUrl = driver.getCurrentUrl();
-		verifyEquals(loginPageUrl,"https://www.facebook.com/");
+		assertEquals(loginPageUrl, "https://www.facebook.com/");
 		
-		
+		System.out.println("Assert 02");
 		log.info("verify equal 02");
 		String loginPageTitle = driver.getTitle();
-		verifyEquals(loginPageTitle, "Facebook – log in or sign up");
+		assertEquals(loginPageTitle, "Facebook – log in or sign up"); 
 		
+		System.out.println("Assert 03");
 		log.info("verify true 03");
-		verifyTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
+		assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
 		
 		
+//		System.out.println("Assert 04");
+//		log.info("verify true 04");
+//		assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
+//		
+
+		 
 	}
 
 
